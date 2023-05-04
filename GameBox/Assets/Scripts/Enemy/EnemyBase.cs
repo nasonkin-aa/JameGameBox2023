5263bf5b0af391811030d635fc91c84e9fca7fe1
Native <Native095@yandex.ru>
2023-05-04 19:39:18 +1000

    Add raycast for enemys, take damage for player
    Враги не атакуют через препятствия, персонаж получает урон

--- GameBox/Assets/Scripts/Enemy/EnemyBase.cs
+++ GameBox/Assets/Scripts/Enemy/EnemyBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 
 public class EnemyBase : MonoBehaviour
 {
@@ -15,13 +16,23 @@ public class EnemyBase : MonoBehaviour
     [SerializeField]
     protected PolygonCollider2D _attackCollider;
 
+    [SerializeField]
+    protected float _attackkDistance = 1f;
+
+    [SerializeField]
     protected float _rotationSpeed = 180f;
+
+    [SerializeField]
+    protected float _hp = 1;
+
+    [SerializeField]
+    protected float _damage = 10;
 
     protected Rigidbody2D _rb;
     protected Collider2D _targetCollider;
-    protected float _hp = 1;
     protected States _state = States.Moving;
     protected NavMeshAgent agent;
+    protected Character _characterScript;
 
     protected Vector2 _previousePos;
     protected enum States
@@ -45,14 +56,18 @@ public class EnemyBase : MonoBehaviour
         agent.updateUpAxis = false;
         _rb = transform.GetComponent<Rigidbody2D>();
         _targetCollider = _target.GetComponent<Collider2D>();
-        _previousePos = (Vector2)transform.position;
+        _previousePos = transform.position;
+        _characterScript = 
+            _target.GetComponent<Character>() != null ? 
+            _target.GetComponent<Character>() : 
+            _target.gameObject.AddComponent<Character>();
     }
 
 
     protected virtual void Update()
     {
         if (_state == States.Moving)
-            LookAtTarget();
+            LookAtTargetInMoovment(_rotationSpeed);
     }
 
     protected virtual void FixedUpdate()
@@ -62,51 +77,60 @@ public class EnemyBase : MonoBehaviour
 
     protected virtual void MoveToTarget()
     {
-        /*  
-                Vector3 directionToTarget = (_target.position - transform.position).normalized;
-                _rb.velocity = directionToTarget * _speed;*/
         agent.SetDestination(_target.position);
     }
 
-    protected virtual void LookAtTarget()
+    protected virtual void LookAtTargetInMoovment(float rotationSpeed)
     {
-        
         Vector2 moveDerection = (Vector2)transform.position - _previousePos;
 
         float angle = Mathf.Atan2(moveDerection.y, moveDerection.x) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        _previousePos = transform.position;
+    }
+
+    protected virtual void LookAtTarget(float rotationSpeed)
+    {
+        Vector2 targetDerection = _target.position - transform.position;
+
+        float angle = Mathf.Atan2(targetDerection.y, targetDerection.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         _previousePos = transform.position;
     }
 
 
     protected virtual IEnumerator Attack()
     {
-        if (_state == States.Attacking)
-        {
-            yield break;
-        }
-
-        _state = States.Attacking;
-        agent.speed = 0;
         yield return new WaitForSeconds(1); // под анмации
-        agent.speed = 3;
 
+        if (_attackCollider.IsTouching(_targetCollider))
+        {
+            //Debug.Log("Персонажа задело");
+            DealDamage();
+        }
+        //Debug.Log("Атака закончилась");
+        agent.speed = 3;
         _state = States.Moving;
     }
 
     protected virtual void StateMachine()
-    {
-        if (_attackCollider.IsTouching(_targetCollider))
-        {
-            _rb.velocity = Vector3.zero;
-            StartCoroutine(Attack());
-        }
-
+    {        
         if (_state == States.Moving)
-        {
             MoveToTarget();
+
+        if (_state != States.Attacking && DetectTarget()  )
+        {
+            //Debug.Log("Пошла атака");
+            LookAtTarget(_rotationSpeed * 100); // костыль, резкий поворот
+            _state = States.Attacking;
+            _rb.velocity = Vector3.zero;
+            agent.speed = 0;
+            StartCoroutine(Attack());
+
         }
     }
 
@@ -117,5 +141,30 @@ public class EnemyBase : MonoBehaviour
         _state = States.Dead;
 
         yield return new WaitForSeconds(1); // под анмации      
+    }
+
+    protected virtual bool DetectTarget ()
+    {
+        List<RaycastHit2D> resultsOfHit = new List<RaycastHit2D>();
+        Vector2 targetDirection = (_target.position - transform.position).normalized;
+        int numberOfHits = Physics2D.Raycast(transform.position, targetDirection, new ContactFilter2D().NoFilter(), resultsOfHit, _attackkDistance);
+        Debug.DrawLine(transform.position, (Vector2)transform.position + targetDirection * _attackkDistance, Color.red);
+
+        
+
+        // Проверяем, попал ли луч в объект
+        if (resultsOfHit.Any(hit => hit.transform == _target && !resultsOfHit.Any(hit => hit.transform.tag == "Wall"))) // поменять условие
+        {
+            //Debug.Log(numberOfHits);
+            //Debug.Log("персонаж замечен");
+            //Debug.Log(_state);
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void DealDamage()
+    {
+        _characterScript.GetDamage(_damage);
     }
 }
