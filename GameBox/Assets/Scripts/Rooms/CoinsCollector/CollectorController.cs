cc6826c3e1d79be2b96b0b1293153bccaba83373
Native <Native095@yandex.ru>
2023-05-07 03:11:26 +1000

    Add Coin collect lvl

---
+++ GameBox/Assets/Scripts/Rooms/CoinsCollector/CollectorController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectorController : IRoomManagerBase
+{
+    private Character character;
+
+    private void Start()
+    {
+        character = FindObjectOfType<Character>();
+        character.OnDie.AddListener(CharacterDie);
+        character.OnTakeCoin.AddListener(CoinCollected);
+    }
+    public override void CharacterDie()
+    {
+        character.OnDie.RemoveListener(CharacterDie);
+        OnCharacterDie.Invoke();
+    }
+
+    public override void CharacterEnter(int targetCount)
+    {
+        OnStart.Invoke(CountToTarget);
+    }
+
+    public override void CharacterEnter()
+    {
+        OnStart.Invoke(CountToTarget);
+    }
+
+    public override void Failed()
+    {
+        OnLose.Invoke();
+    }
+
+    public override void Finished()
+    {
+        OnFinish.Invoke();
+    }
+
+    public override void LevelProgress(int count)
+    {
+        OnLevelProgress.Invoke(count);
+    }
+
+    private void CoinCollected()
+    {
+        _targetNow++;
+        LevelProgress(_targetNow);
+
+
+        if (_targetNow == CountToTarget)
+            Finished();
+    }
+}
